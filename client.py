c984946acebcea1ecd1e4f077479c6f4b4af9a88
anthonycope <anthonycope@mail.usf.edu>
2013-10-13 23:10:23 -0400

    Added basic client-server functionality.

---
+++ client.py
@@ -0,0 +1,17 @@
+#client.py
+#Anthony Cope and Matthew Weis
+
+#not sure if we have to put the ridiculous header here or not
+
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
+
